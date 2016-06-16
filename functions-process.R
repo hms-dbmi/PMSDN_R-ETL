836fa0c44fbc45c68bb63f1a9ed0925e223c8a16
Tom <Tom@dbmitdesainsmbp.private.wireless.med.harvard.edu>
2016-06-16 12:52:19 -0400

    cvb

--- functions-process.R
+++ functions-process.R
@@ -27,10 +27,12 @@ processFile <- function(questionnaire, noOutput = F)
 
   # Process each SubFile level (excluding the empty SubFile level->Demographics)
   for (subfile in levels(factor(premap$SubFile, exclude = "")))
-      data2 <- merge(data2, processSubfile(questionnaire, subfile, data, premap, noOutput = noOutput), by = "Patient.ID")
-
-      print("Finished subfile")
-
+      data2 <- tryCatch({merge(data2, processSubfile(questionnaire, subfile, data, premap, noOutput = noOutput), by = "Patient.ID")
+        },
+        error = function(e){
+          dump(data2, file=paste0(subfile,"dump.asc"))
+        }
+  )
   if (!noOutput)
     ontology <<- pop(ontology)
 
