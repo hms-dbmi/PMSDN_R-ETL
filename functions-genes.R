5d7f4515348afa973546906f1a5cc9f221c054a1
Maxime Wack <maximewack@free.Fr>
2019-01-14 06:37:15 +0100

    Moved file downloads to 00

--- functions-genes.R
+++ functions-genes.R
@@ -163,12 +163,3 @@ liftOver <- function(genetics)
 
   select(genetics, -rowname)
 }
-
-downloadExternalFiles <- function()
-{
-  # LiftOver
-  system("wget -O liftOver http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/liftOver")
-  system("wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz -O - | gunzip > hg19ToHg38.over.chain")
-  system("wget http://hgdownload.soe.ucsc.edu/goldenPath/hg18/liftOver/hg18ToHg38.over.chain.gz -O - | gunzip > hg18ToHg38.over.chain")
-  system("wget http://hgdownload.soe.ucsc.edu/goldenPath/hg17/liftOver/hg17ToHg19.over.chain.gz -O - | gunzip > hg17ToHg19.over.chain")
-}
