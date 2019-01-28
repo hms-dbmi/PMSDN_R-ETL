a2ce0ab2e62f3357e4199d2d4605caded505e2a8
Maxime Wack <maximewack@free.Fr>
2019-01-28 03:41:33 +0100

    Prepare genetics remade from scratch for the new genetic results file format

--- 04_prepare_genetics.R
+++ 04_prepare_genetics.R
@@ -1,151 +1,78 @@
-# source("functions-loading.R")
 library(tidyverse)
-library(lubridate)
 
-# Extract the genetic test results fields from the clinical file, only where there are results
-read.csv("dataGenetic.csv") %>%
+# Read genetic file and select relevant columns, in correct ordering
+# Discard columns that could contain identifiable data
+# Discard columns that don't present analytical usability
+read_csv("dataGenetic.csv") -> genetic
+
+genetic %>%
   select(Patient.ID,
-         Test.Date,
-         Genetic.Status,
-         Genomic.Position.Start.Min,
-         Genomic.Position.End.Min,
-         Genomic.Position.Start.Max,
-         Genomic.Position.End.Max,
-         Participant.Origin,
-         Comments,
-         Std.Nomenclature,
-         Test.Method,
-         Genome.Browser.Build,
-         Gene,
-         Lab,
-         Category,
-         Test.Verification,
-         `Results.Verified.-.Consultant`,
-         Karyotype.Start,
-         Karyotype.End,
-         Array.Version,
-         Array.Confirmation.Studies,
-         `Start.Exon/Intron`:Parental.Origin,
-         `Other.Gain/Loss.1`,
-         `Other.Arm.Gain/Loss.1`,
-         Other.Position.Start.1,
-         Other.Position.End.1,
-         Origin.1,
-         `Other.Gain/Loss.2`,
-         `Other.Arm.Gain/Loss.2`,
-         Other.Position.Start.2,
-         Other.Position.End.2,
-         Origin.2,
-         `Other.Gain/Loss.3`,
-         `Other.Arm.Gain/Loss.3`,
-         Other.Position.Start.3,
-         Other.Position.End.3,
-         Origin.3) %>%
-  filter(Genetic.Status != "No Results Received") %>%
-# Parse the dates
-  mutate(Test.Date = gsub("/", "-",                      Test.Date, perl = T)) %>%
-  mutate(Test.Date = gsub("^(\\d)-", "0\\1-",            Test.Date, perl = T)) %>%
-  mutate(Test.Date = gsub("-(\\d)-", "-0\\1-",           Test.Date, perl = T)) %>%
-  mutate(Test.Date = gsub("-(\\d{2})$", "-20\\1",        Test.Date, perl = T)) %>%
-  mutate(Test.Date = gsub("(^\\d{2}-\\d{4}$)", "01-\\1", Test.Date, perl = T)) %>%
-  mutate(Test.Date = gsub("^[\\w ]+$", "",               Test.Date, perl = T)) %>%
-  mutate(Test.Date = mdy(Test.Date)) %>%
-  distinct -> Genetics
+         GeneticStatus,
+         SHANK3Involved,
+         PathogenicSHANK3Defect,
+         # CategoryChr22,
+         # SubCategoryChr22,
+         Mosaicism,
+         Origin,
+         TestVerification,
+         # ParentTestMethod,
+         # ParentResults,
+         # starts_with("Karyotype"),
+         # starts_with("FISH"),
+         Array,
+         starts_with("Array1"),
+         # starts_with("Sequencing"),
+         Sequencing,
+         # GenomicChange,
+         # CodingDNAChange,
+         # ProteinChange,
+         SequencingEffect = Effect,
+         SequencingClinicalSignificance = ClinicalSignificance,
+         # SequencingOtherAbnormalities,
+         -ends_with("Lab"),
+         -ends_with("Version"),
+         -ends_with("Method"),
+         -contains("Other"),
+         -contains("MAX")) %>%
+# Ensure years are numeric
+  mutate_at(vars(ends_with("Year")), as.numeric) %>%
+# Keep only verified results (should select all results now)
+  filter(GeneticStatus == "Results verified") %>%
+# Fix some category levels.
+# (-> Mosaicism and Origin should be fixed upstream)
+  mutate(Mosaicism        = Mosaicism        %>% fct_collapse("Yes"                   = c("Yes", "Yes (duplication)")),
+         Origin           = Origin           %>% fct_collapse("De novo"               = c("De novo", "De Novo"),
+                                                             "Unknown"                = c("Unknown", "Unknown (see CommentsCB)"),
+                                                             "Unknown - Not maternal" = c("Unknown - Not maternal", "Unknown (Not maternal)"),
+                                                             "Unknown - Not paternal" = c("Unknown - Not paternal", "Unknown - Not Paternal", "Unknown (Not paternal)", "Unknown (not paternal)")),
+         TestVerification = TestVerification %>% fct_collapse("Complete"              = genetic$TestVerification %>% keep(~str_detect(., "^Complete")) %>% unique,
+                                                             "Incomplete"             = genetic$TestVerification %>% keep(~str_detect(., "^Incomplete")) %>% unique)) -> genetic
 
-# Correct variable names
-names(Genetics) <- gsub("/", ".", names(Genetics))
+# Prepare names for reshaping and transmart mapping
+genetic %>%
+  set_names(function(x)
+            {
+              x %>%
+                str_replace("^Array1_", "Array.") %>%
+                str_replace("Chr22Abnl_", "") %>%
+                str_replace("MIN", "") %>%
+                str_replace("(\\d)_GRCh38hg38", ".latest\\1") %>%
+                str_replace("(\\d)$", "_\\1")
+            }) -> genetic
 
-# Correct test dates in the 20th century
-Genetics$Test.Date[which(Genetics$Test.Date > as.Date(now()))] <- Genetics$Test.Date[which(Genetics$Test.Date > as.Date(now()))] - years(100)
+# Reshape
+genetic %>%
+  filter(Array == "Yes") %>%
+  reshape(direction = "long",
+          varying = genetic %>% names %>% str_detect("_\\d$"),
+          sep = "_",
+          timevar = "Array.num") %>%
+  filter(!Array.Type %>% is.na) %>%
+  mutate(Result.type = "Array") %>%
+  select(-id, -starts_with("Sequencing"), -Array) %>%
+  bind_rows(genetic %>%
+              filter(Sequencing == "Yes") %>%
+              mutate(Result.type = "Sequencing") %>%
+              select(-starts_with("Array"), -Sequencing)) -> genetic
 
-# Sort by patient and date
-Genetics %>%
-  arrange(Patient.ID, Test.Date) %>%
-# Create new variables with defaults and reorder the columns
-  mutate(Result.type = "coordinates",
-         Gain_Loss.1 = "Loss",
-         Chr_Gene.1 = "22",
-         Start.1 = Genomic.Position.Start.Min,
-         End.1 = Genomic.Position.End.Min,
-         Origin.1 = Participant.Origin,
-         Gain_Loss.2 = Other.Gain.Loss.1,
-         Chr_Gene.2 = Other.Arm.Gain.Loss.1,
-         Start.2 = Other.Position.Start.1,
-         End.2 = Other.Position.End.1,
-         Origin.2 = Origin.1,
-         Gain_Loss.3 = Other.Gain.Loss.2,
-         Chr_Gene.3 = Other.Arm.Gain.Loss.2,
-         Start.3 = Other.Position.Start.2,
-         End.3 = Other.Position.End.2,
-         Origin.3 = Origin.2,
-         Gain_Loss.4 = Other.Gain.Loss.3,
-         Chr_Gene.4 = Other.Arm.Gain.Loss.3,
-         Start.4 = Other.Position.Start.3,
-         End.4 = Other.Position.End.3,
-         Origin.4 = Origin.3) %>%
-# Select only relevant columns
-  select(Patient.ID,
-         Test.Date,
-         Genetic.Status,
-         Comments,
-         Std.Nomenclature,
-         Genomic.Position.Start.Max,
-         Genomic.Position.End.Max,
-         Test.Method,
-         Genome.Browser.Build,
-         Result.type,
-         Gain_Loss.1,
-         Chr_Gene.1,
-         Start.1,
-         End.1,
-         Origin.1,
-         Gain_Loss.2,
-         Chr_Gene.2,
-         Start.2,
-         End.2,
-         Origin.2,
-         Gain_Loss.3,
-         Chr_Gene.3,
-         Start.3,
-         End.3,
-         Origin.3,
-         Gain_Loss.4,
-         Chr_Gene.4,
-         Start.4,
-         End.4,
-         Origin.4,
-         Gene,
-         Lab,
-         Category,
-         Test.Verification,
-         `Results.Verified.-.Consultant`,
-         Karyotype.Start,
-         Karyotype.End,
-         Array.Version,
-         Array.Confirmation.Studies,
-         Start.Exon.Intron:Parental.Origin) -> Genetics
-
-# Delete non-informative test results
-Genetics %>%
-  filter(Genetic.Status == "Results Verified",
-         Test.Method != "Karyotype",
-         Test.Method != "Karyotype/FISH",
-         Test.Method != "FISH") %>%
-# Keep only the latest informative test results
-  group_by(Patient.ID) %>%
-  filter(Test.Date == last(Test.Date)) %>%
-  ungroup -> Genetics
-
-# Small corrections
-Genetics$Chr_Gene.2 <- gsub("[pq]$", "", Genetics$Chr_Gene.2)
-Genetics$Chr_Gene.3 <- gsub("[pq]$", "", Genetics$Chr_Gene.3)
-Genetics$Chr_Gene.4 <- gsub("[pq]$", "", Genetics$Chr_Gene.4)
-Genetics$Result.type[Genetics$Test.Method == "Bi-Directional Sequence Analysis" | Genetics$Test.Method == "Sequence Analysis" | Genetics$Test.Method == "Whole exome sequencing"] <- "mutation"
-
-# Select out irrelevant variables
-Genetics <- select(Genetics, -Genetic.Status)
-
-Genetics %>%
-  mutate(Test.Date = format(Genetics$Test.Date, format = "%m/%d/%y %I:%M %p")) -> Genetics
-
-write.csv(Genetics, "dataGenetics.csv", na = "", row.names = F)
+write_csv("dataGenetic.csv")
