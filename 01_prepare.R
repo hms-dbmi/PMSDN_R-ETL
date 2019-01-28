0a383100863f83b70abf010f845bf126baa2b5e2
Maxime Wack <maximewack@free.Fr>
2019-01-28 03:40:52 +0100

    Added manual steps for the genetic results file and recommandations

--- 01_prepare.R
+++ 01_prepare.R
@@ -8,6 +8,18 @@ str_c("data", files) %>%
   walk(html2csv)
 
 # Transform genetic results from XLSX to CSV
+#
+# A couple things to do first :
+#
+# Rename the file from Catalina as dataGenetic.xlsx
+# (-> or better, get Calatina to name it that way when she sends it to you)
+#
+# Add any additional case to this file, and check that tho columns match
+# (-> or get Catalina to send a complete file, keeping with the same structure (no additional column, no renaming))
+#
+# Patient ID 10370 has multiple misplaced cells : Array2_Year, Array2_Lab, Array2_Method, Array2_Version, CodingDNAChange, ProteinChange, and Effect. For now this is censored.
+# (-> get Catalina to fix this in her file)
+
 read_xlsx("dataGenetic.xlsx") %>%
   rename(Patient.ID = ID) %>%
   filter(!Patient.ID %>% is.na) %>%
