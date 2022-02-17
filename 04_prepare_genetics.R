03d688b85a49e6805c4425d91ff6151a4460964f
Rayin-saber <54319233+Rayin-saber@users.noreply.github.com>
2022-02-17 09:47:54 -0500

    Update 04_prepare_genetics.R

--- 04_prepare_genetics.R
+++ 04_prepare_genetics.R
@@ -34,31 +34,45 @@ genetic %>%
          -ends_with("Method"),
          -contains("Other"),
          -contains("MAX")) %>%
+
+  # Fix some category levels.
+  # (-> Mosaicism and Origin should be fixed upstream)
+  mutate(Mosaicism        = Mosaicism        %>% fct_collapse("Yes"                   = c("Yes", "Yes (duplication)")),
+         Origin           = Origin           %>% fct_collapse("De novo"               = c("De novo", "De Novo"),
+                                                              "Unknown"                = c("Unknown", "Unknown (see CommentsCB)"),
+                                                              "Unknown - Not maternal" = c("Unknown - Not maternal", "Unknown (Not maternal)"),
+                                                              "Unknown - Not paternal" = c("Unknown - Not paternal", "Unknown - Not Paternal", "Unknown (Not paternal)", "Unknown (not paternal)")),
+         TestVerification = TestVerification %>% fct_collapse("Complete"              = genetic$TestVerification %>% keep(~str_detect(., "^Complete")) %>% unique,
+                                                              "Incomplete"             = genetic$TestVerification %>% keep(~str_detect(., "^Incomplete")) %>% unique)) %>%
 # Ensure years are numeric
   mutate_at(vars(ends_with("Year")), as.numeric) %>%
 # Keep only verified results (should select all results now)
-  filter(GeneticStatus == "Results verified") %>%
-# Fix some category levels.
-# (-> Mosaicism and Origin should be fixed upstream)
-  mutate(Mosaicism        = Mosaicism        %>% fct_collapse("Yes"                   = c("Yes", "Yes (duplication)")),
-         Origin           = Origin           %>% fct_collapse("De novo"               = c("De novo", "De Novo"),
-                                                             "Unknown"                = c("Unknown", "Unknown (see CommentsCB)"),
-                                                             "Unknown - Not maternal" = c("Unknown - Not maternal", "Unknown (Not maternal)"),
-                                                             "Unknown - Not paternal" = c("Unknown - Not paternal", "Unknown - Not Paternal", "Unknown (Not paternal)", "Unknown (not paternal)")),
-         TestVerification = TestVerification %>% fct_collapse("Complete"              = genetic$TestVerification %>% keep(~str_detect(., "^Complete")) %>% unique,
-                                                             "Incomplete"             = genetic$TestVerification %>% keep(~str_detect(., "^Incomplete")) %>% unique)) -> genetic
+  filter(GeneticStatus == "Results verified" | GeneticStatus == "Results Verified")  -> genetic
+
+# #Prepare names for reshaping and transmart mapping
+# genetic %>%
+#   set_names(function(x)
+#             {
+#               x %>%
+#                 str_replace("^Array1_", "Array.") %>%
+#                 str_replace("Chr22Abnl_", "") %>%
+#                 str_replace("MIN", "") %>%
+#                 str_replace("(\\d)_GRCh38hg38", ".latest\\1") %>%
+#                 str_replace("(\\d)$", "_\\1")
+#             }) -> genetic
 
 # Prepare names for reshaping and transmart mapping
-genetic %>%
-  set_names(function(x)
-            {
-              x %>%
-                str_replace("^Array1_", "Array.") %>%
-                str_replace("Chr22Abnl_", "") %>%
-                str_replace("MIN", "") %>%
-                str_replace("(\\d)_GRCh38hg38", ".latest\\1") %>%
-                str_replace("(\\d)$", "_\\1")
-            }) -> genetic
+rename <- function(x)
+  {
+    x %>%
+      str_replace("^Array1_", "Array.") %>%
+      str_replace("Chr22Abnl_", "") %>%
+      str_replace("MIN", "") %>%
+      str_replace("(\\d)_GRCh38hg38", ".latest\\1") %>%
+      str_replace("(\\d)$", "_\\1")
+  }
+genetic_name <- rename(colnames(genetic))
+genetic %>% set_names(genetic_name) -> genetic
 
 # Reshape
 genetic %>%
